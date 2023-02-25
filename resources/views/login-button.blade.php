43d1d13be1bfcb510ea2a6b002843d8ef21b09d2
Dan Palmieri <dlspalmieri@gmail.com>
2023-02-25 18:55:15 -0300

    fix button

--- resources/views/login-button.blade.php
+++ resources/views/login-button.blade.php
@@ -1,17 +1,21 @@
 @if(config('eduzz-account.enabled'))
     <script src="https://cdn.eduzzcdn.com/accounts/accounts.js"></script>
     <script>
-        window.Eduzz.Accounts.login("{{ config('eduzz-account.id') }}", {
-                logo: "{{ config('eduzz-account.id') }}",
-                bg: "{{ config('eduzz-account.background.image') }}",
-                btnColor: "{{ config('eduzz-account.button.color') }}",
-                dark: false,
-                @if(app()->environment('local')) env: 'homolog' @endif
-            })
-            .subscribe((data) => {
-                document.getElementById('eduzz-account-button').setAttribute('href', "{{ config('eduzz-account.callback.url') }}" + data + "?{!! http_build_query($_GET) !!}");
-            });
+        document.addEventListener("DOMContentLoaded", function(event) {
+            function loginEduzzAccount() {
+                window.Eduzz.Accounts.login("{{ config('eduzz-account.id') }}", {
+                        logo: "{{ config('eduzz-account.id') }}",
+                        bg: "{{ config('eduzz-account.background.image') }}",
+                        btnColor: "{{ config('eduzz-account.button.color') }}",
+                        dark: false,
+                        @if(app()->environment('local')) env: 'homolog' @endif
+                    })
+                    .subscribe((data) => {
+                        window.location.href = "/eduzz/validate/" + data + "?{!! http_build_query($_GET) !!}"
+                    });
+            }
+        });
     </script>
 
-    <a id="eduzz-account-button">{{ $slot ?? 'Login com Eduzz Account' }}</a>
+    <a id="eduzz-account-button" onclick="loginEduzzAccount()">{{ $slot ?? 'Login com Eduzz Account' }}</a>
 @endif
