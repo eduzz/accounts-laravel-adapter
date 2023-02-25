8de18dfb82292266a873121e32bd54b1b89e885e
Dan Palmieri <dlspalmieri@gmail.com>
2023-02-25 18:43:59 -0300

    fix button

--- src/Components/LoginButton.php
+++ src/Components/LoginButton.php
@@ -21,6 +21,6 @@ class LoginButton extends Component
      */
     public function render(): View|Closure|string
     {
-        return view('components.login-button');
+        return view('laravel-eduzz-account::login-button');
     }
 }
