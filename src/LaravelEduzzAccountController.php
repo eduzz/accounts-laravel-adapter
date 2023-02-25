19235b3a96036bb8d10a80652b98b3925dadb0f4
danpalmieri <danpalmieri@users.noreply.github.com>
2023-02-25 15:37:13 +0000

    Fix styling

--- src/LaravelEduzzAccountController.php
+++ src/LaravelEduzzAccountController.php
@@ -2,24 +2,24 @@
 
 namespace Eduzz\LaravelEduzzAccount;
 
-use Illuminate\Routing\Controller as BaseController;
-use Illuminate\Http\Request;
 use App\Models\User;
 use Http;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller as BaseController;
 
-class LaravelEduzzAccountController extends BaseController {
-
+class LaravelEduzzAccountController extends BaseController
+{
     public function __invoke(Request $request, $token)
     {
         $url = app()->environment('local')
-            ? "https://accounts-api.qa.devzz.ninja/validate"
-            : "https://accounts-api.eduzz.com/validate";
+            ? 'https://accounts-api.qa.devzz.ninja/validate'
+            : 'https://accounts-api.eduzz.com/validate';
 
         $request = Http::asJson()->post($url, [
-                'partner' => config('eduzz-account.id'),
-                'secret' => config('eduzz-account.secret'),
-                'token' => $token,
-            ]);
+            'partner' => config('eduzz-account.id'),
+            'secret' => config('eduzz-account.secret'),
+            'token' => $token,
+        ]);
 
         $response = json_decode($request->getBody()->getContents());
 
@@ -29,17 +29,17 @@ class LaravelEduzzAccountController extends BaseController {
         if (isset($response?->user)) {
             $user = User::where(config('eduzz-account.table.column'), $response->user->eduzzIds[0])->first();
 
-            if(! $user){
+            if (! $user) {
                 $user = User::create([
                     'name' => $response->user->name,
                     'email' => $response->user->email,
-                    config('eduzz-account.table.column') => $response->user->eduzzIds[0]
+                    config('eduzz-account.table.column') => $response->user->eduzzIds[0],
                 ]);
 
-                if(config('eduzz-account.has_teams')){
+                if (config('eduzz-account.has_teams')) {
                     $user->ownedTeams()->save(Team::forceCreate([
                         'user_id' => $user->id,
-                        'name' => __('Time de ') . explode(' ', $user->name, 2)[0],
+                        'name' => __('Time de ').explode(' ', $user->name, 2)[0],
                         'personal_team' => true,
                     ]));
                 }
@@ -52,5 +52,4 @@ class LaravelEduzzAccountController extends BaseController {
             return redirect(config('eduzz-account.redirect_to'));
         }
     }
-
 }
