cebfc4441b5d1600d63e41daa5ce40619980ed71
Dan Palmieri <dlspalmieri@gmail.com>
2023-02-25 18:58:51 -0300

    fix button

--- resources/views/login-button.blade.php
+++ resources/views/login-button.blade.php
@@ -1,20 +1,18 @@
 @if(config('eduzz-account.enabled'))
     <script src="https://cdn.eduzzcdn.com/accounts/accounts.js"></script>
     <script>
-        document.addEventListener("DOMContentLoaded", function(event) {
-            function loginEduzzAccount() {
-                window.Eduzz.Accounts.login("{{ config('eduzz-account.id') }}", {
-                        logo: "{{ config('eduzz-account.id') }}",
-                        bg: "{{ config('eduzz-account.background.image') }}",
-                        btnColor: "{{ config('eduzz-account.button.color') }}",
-                        dark: false,
-                        @if(app()->environment('local')) env: 'homolog' @endif
-                    })
-                    .subscribe((data) => {
-                        window.location.href = "/eduzz/validate/" + data + "?{!! http_build_query($_GET) !!}"
-                    });
-            }
-        });
+        function loginEduzzAccount() {
+            window.Eduzz.Accounts.login("{{ config('eduzz-account.id') }}", {
+                    logo: "{{ config('eduzz-account.id') }}",
+                    bg: "{{ config('eduzz-account.background.image') }}",
+                    btnColor: "{{ config('eduzz-account.button.color') }}",
+                    dark: false,
+                    @if(app()->environment('local')) env: 'homolog' @endif
+                })
+                .subscribe((data) => {
+                    window.location.href = "/eduzz/validate/" + data + "?{!! http_build_query($_GET) !!}"
+                });
+        }
     </script>
 
     <a id="eduzz-account-button" onclick="loginEduzzAccount()">{{ $slot ?? 'Login com Eduzz Account' }}</a>
