1f3558a22bb73366f1d388548c5b00f7e5bf0111
Dan Palmieri <dlspalmieri@gmail.com>
2023-02-26 15:34:40 -0300

    fix route

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get(config('eduzz-account.callbackUrl'), config('eduzz-account.routeController'));
+Route::any(config('eduzz-account.callbackUrl'), config('eduzz-account.routeController'));
 Route::view('redirect-eduzz-account', 'eduzz-account::redirect');
