74913bdcf18217f3b51b7584b70b871a67ff689a
Dan Palmieri <dlspalmieri@gmail.com>
2023-03-08 16:26:17 -0300

    fix namespace

--- src/AccountsLaravelAdapterServiceProvider.php
+++ src/AccountsLaravelAdapterServiceProvider.php
@@ -16,7 +16,7 @@ class AccountsLaravelAdapterServiceProvider extends PackageServiceProvider
          */
         $package
             ->name('accounts-laravel-adapter')
-            ->hasConfigFile()
+            ->hasConfigFile('accounts-laravel-adapter')
             ->hasViews()
             ->hasViewComponent('eduzz-account', Components\LoginButton::class)
             ->hasRoute('web')
